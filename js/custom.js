8c1578d5310edfcbeac2ca188d873952d4a8ce45
=tutstricks <=>
2021-04-16 05:38:54 -0700

    Before Chart

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 	/*var today = new Date();
 	var dd = String(today.getDate()).padStart(2, '0');
@@ -15,8 +17,12 @@ $(document).ready(function(){
 	var total_tarritories = $("#total_tarritories");
 	var daily_vaccined = $("#daily_vaccined");
 	var fully_vaccined = $("#fully_vaccined");
+	var single_fully_vaccined = 0;
+	var single_daily_vaccined = 0;
 
 	// $("#getdata").click(function(){
+
+	
 		$.ajax({
 			url:"https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json",
 			success:function(resp){
@@ -30,19 +36,36 @@ $(document).ready(function(){
 					}else{
 						vaccined = ourSignleData[ourSignleData.length -1].total_vaccinations;
 					}
+
 					if(ourSignleData[ourSignleData.length -1].people_fully_vaccinated){
-						fullyVaccinced =fullyVaccinced + ourSignleData[ourSignleData.length -1].people_fully_vaccinated;
+						single_fully_vaccined = ourSignleData[ourSignleData.length -1].people_fully_vaccinated
+						
 					}
 					if(ourSignleData[ourSignleData.length -1].daily_vaccinations){
-						dailyVaccined =dailyVaccined + ourSignleData[ourSignleData.length -1].daily_vaccinations;
+						single_daily_vaccined = ourSignleData[ourSignleData.length -1].daily_vaccinations
+						
 					}
 					
-					newData = `<div class='single_data'>
-						<p>${josnData[i].country} ${vaccined}</p>
+					newData = `<div class='col-lg-4 col-md-4 col-sm-6 single_data'>
+						<p class='country_heading'>${josnData[i].country}</p>
+						<table class='signle_data_table'>
+							<tr class='table_heading'>
+								<td>Total</td>
+								<td>Daily</td>
+								<td>Fully</td>
+							</tr>
+							<tr>
+								<td>${vaccined}</td>
+								<td>${single_daily_vaccined}</td>
+								<td>${single_fully_vaccined}</td>
+							</tr>
+						</table>
 					</div>`;
 					ourData.append(newData);
 					
 					total_vaccined_peoples = total_vaccined_peoples + vaccined;
+					fullyVaccinced =fullyVaccinced + single_fully_vaccined;
+					dailyVaccined =dailyVaccined + single_daily_vaccined;
 					
 
 					// console.log(total_vaccined_peoples)
