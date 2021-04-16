87abde7def44e287d5c789f8e0b4e2f74b7fe4de
tutstricks <tutstricks@gmail.com>
2021-04-17 01:38:33 +0500

    Added favicon

--- js/custom.js
+++ js/custom.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 
 	// $("#getdata").click(function(){
 
-	
+	setInterval(function(){
 		$.ajax({
 			url:"https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json",
 			success:function(resp){
@@ -86,6 +86,7 @@ $(document).ready(function(){
 				console.log(josnData);
 			}
 		});
+	},1000);
 
 	// });
 });
